7b096a202c86e3eb997bf37af47b95da17b0fe32
nikkishah-patel <nikkishah.ap@gmail.com>
2018-02-05 09:15:08 -0500

    added lab10

---
+++ src/CarClass.java
@@ -0,0 +1,68 @@
+
+public class CarClass {
+
+	
+		//Display a list of at least 6 cars - 3 new and 3 used
+		//let the user select one of the cars
+		//re-display the info for car selected
+		//ask the user if they want to buy the car-- if they do, remove the car
+		// re-display the list
+
+private String carMake;
+private String carModel;
+private int carYear;
+private double carPrice;
+
+
+public CarClass(String make, String model, int year, double price) {
+	this.make = make;
+	this.model = model;
+	this.year = year;
+	this.price = price; 
+	}
+
+public CarClass() {
+name = "";
+model = "";
+year = 0;
+price = 0.0;
+}
+	
+	public String getCarMake() {
+	return carMake;
+}
+
+public void setCarMake(String carMake) {
+	this.carMake = carMake;
+}
+
+public String getCarModel() {
+	return carModel;
+}
+
+public void setCarModel(String carModel) {
+	this.carModel = carModel;
+}
+
+public int getCarYear() {
+	return carYear;
+}
+
+public void setCarYear(int carYear) {
+	this.carYear = carYear;
+}
+
+public double getCarPrice() {
+	return carPrice;
+}
+
+public void setCarPrice(double carPrice) {
+	this.carPrice = carPrice;
+}
+	int i = 0; i <6; i++) {
+		
+	
+}
+
+
+}}
